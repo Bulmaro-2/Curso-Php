e68f5ff685b861346c8504945e8b0895dacaeb26
Bulmaro-2 <bulmaro_logo@outlook.com>
2021-04-23 16:34:08 -0500

    Creacion Conexio PDO

--- CRUD MVC/Model/ConexionBD.php
+++ CRUD MVC/Model/ConexionBD.php
@@ -1,5 +1,8 @@
 <?php
     class ConexionBD{
-        
+        public function CBD(){
+            $BD = new PDO("mysql:host=127.0.0.1;dbname=crud","root","");//realizar la conexion a las bd
+            return $BD;
+        }
     }
 ?>
